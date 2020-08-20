dc1812377e23e1e4cf81ac145f407eb22c2a64eb
unknown <32291941+itsMikeLowrey@users.noreply.github.com>
2020-08-20 01:28:19 -0400

    Create Repo

---
+++ tests/e2e/test.spec.js
@@ -0,0 +1,42 @@
+const chai = require('chai')
+const testWithSpectron = require('vue-cli-plugin-electron-builder/lib/testWithSpectron')
+const chaiAsPromised = require('chai-as-promised')
+const spectron = require('spectron')
+
+chai.use(chaiAsPromised)
+chai.should()
+
+describe('Application launch', function () {
+  this.timeout(100000)
+
+  beforeEach(function () {
+    return testWithSpectron(spectron).then(instance => {
+      this.app = instance.app
+      this.stopServe = instance.stopServe
+    })
+  })
+
+  beforeEach(function () {
+    chaiAsPromised.transferPromiseness = this.app.transferPromiseness
+  })
+
+  afterEach(function () {
+    if (this.app && this.app.isRunning()) {
+      return this.stopServe()
+    }
+  })
+
+  it('opens a window', function () {
+    return this.app.client
+      .getWindowCount()
+      .should.eventually.have.at.least(1)
+      .browserWindow.isMinimized()
+      .should.eventually.be.false.browserWindow.isVisible()
+      .should.eventually.be.true.browserWindow.getBounds()
+      .should.eventually.have.property('width')
+      .and.be.above(0)
+      .browserWindow.getBounds()
+      .should.eventually.have.property('height')
+      .and.be.above(0)
+  })
+})
